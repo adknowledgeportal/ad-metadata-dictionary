e9d1e344390e5f8414da81182c85f326bad786fd
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2023-09-14 12:10:57 -0700

    replicate dictionary table

--- app.py
+++ app.py
@@ -3,22 +3,56 @@ from shiny import App, Inputs, Outputs, Session, reactive, render, req, ui
 
 # get live AD.model.csv from adkp/data-models repo
 url = "https://raw.githubusercontent.com/adknowledgeportal/data-models/main/AD.model.csv"
-model = pd.read_csv(url)
+model_df = pd.read_csv(url)
 
-# drop a few columns
-model = model.drop(['DependsOn Component', 'Properties', 'Required'], axis = 1)
+# remove template rows
+model_df = model_df.loc[model_df["Parent"] != "DataType"]
+
+# pull out "key" attributes
+keys_df = model_df.loc[model_df["Parent"] == "DataProperty"]
+
+#rename key columns
+keys_df = keys_df.rename(columns = {"Attribute" : "Key",
+                          "Description" : "Key Description",
+                          "columnType" : "Key Type"})
+keys_df = keys_df[["Key", "Key Description", "Key Type"]]
+
+# pull out "value" attributes
+values_df = model_df.loc[model_df["Parent"].isin(keys_df["Key"])]
+
+# rename columns
+values_df = values_df.rename(columns = {"Attribute" : "Value",
+                          "Description" : "Value Description"})
+values_df = values_df[["Value", "Value Description", "Source", "Parent"]]
+
+# join keys and values
+merged_df = values_df.merge(
+    keys_df,
+    how = "left", 
+    left_on = "Parent", 
+    right_on = "Key")
+
+merged_df =merged_df[["Key",
+           "Key Description",
+           "Value",
+           "Value Description",
+           "Source",
+           "Key Type"]]
+
+# sort alphabetically
+sorted_df = merged_df.sort_values(by = ["Key", "Value"], 
+                                  kind = "mergesort",
+                                  ascending = True,
+                                  key = lambda col: col.str.lower())
 
 
+# app ui
 app_ui = ui.page_fluid(
     ui.input_switch("fullwidth", "Take full width", True),
     ui.input_switch("fixedheight", "Fixed height", True),
     ui.output_data_frame("grid"),
-    ui.panel_fixed(
-        ui.output_text_verbatim("detail"),
-        right="10px",
-        bottom="10px",
-    ),
-    class_="p-3",
+
+    class_="p-3"
 )
 
 
@@ -26,13 +60,14 @@ def server(input: Inputs, output: Outputs, session: Session):
     @output
     @render.data_frame
     def grid():
-        height = 350 if input.fixedheight() else None
+        height = 500 if input.fixedheight() else None
         width = "100%" if input.fullwidth() else "fit-content"
         return render.DataTable(
-            model,
+            sorted_df,
             height=height,
             width=width,
             filters=True,
+            summary=True
         )
 
 
